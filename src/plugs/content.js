80b78f8197055d4680d817427579c9c3398b3e5c
Merainy <250840443@qq.com>
2016-06-04 18:16:58 +0800

    --

---
+++ src/plugs/content.js
@@ -0,0 +1,36 @@
+import config from '../common/config'
+export default {
+    sidebar: function(params) {
+        var self = this,
+            module = params.module,
+            uri = config.sidebarUri['catagory']
+        this.$http.get({
+            url: uri
+        }).then(function(res) {
+            var sidebarModel = []
+            res.data.forEach(function(v, i) {
+                var link = '#!/'+ config.admin + module + '/' + v._id + '/' + 'content'
+                v.link = v.link || link
+                sidebarModel.push(v)
+            })
+            self.$set('sidebarRoot', undefined)
+            self.$set('sidebarModel', sidebarModel)
+        })
+    },
+    list: function(params) {
+        var self = this
+        var uri = config.listUri[params.module],
+            pid = params.id
+        this.$set('module', params.module)
+        this.$set('parentId', pid)
+        this.$set('listColumns', config.listColumns[params.module])
+        this.$http.get({
+            url: uri,
+            data: {
+                parentId: pid
+            }
+        }).then(function(res) {
+            self.$set('listData', res.data)
+        })
+    }
+}
--- src/plugs/plugs.js
+++ src/plugs/plugs.js
@@ -1,7 +1,9 @@
+import content from './content'
 import catagory from './catagory'
 import catagory_edit from './catagory_edit'
 
 export default {
     catagory: catagory,
-    catagory_edit: catagory_edit
+    catagory_edit: catagory_edit,
+    content: content
 }
